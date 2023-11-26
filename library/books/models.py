38851e58ab96e27f5531de864a34b5dcd1eb37c5
Alex Petrusevich <apetrusevic046@gmail.com>
2023-11-26 03:52:14 +0300

    feat: created books app, models and api

--- library/books/models.py
+++ library/books/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    publication_year = models.PositiveIntegerField()
+    isbn = models.CharField(max_length=13)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Book'
+        verbose_name_plural = 'Books'
+        db_table = 'book'
+
--- library/library/settings.py
+++ library/library/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'books',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +77,12 @@ WSGI_APPLICATION = 'library.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'library_test',
+        'USER': 'root',
+        'PASSWORD': 'alex11',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
